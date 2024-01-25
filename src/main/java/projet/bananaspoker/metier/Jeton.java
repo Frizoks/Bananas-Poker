5006f068dcc0ff364631e49f32c19ddc01a9dbb3
Luc Lecarpentier <luc.lecarpentier5@gmail.com>
2024-01-25 21:49:48 +0100

    jeton

--- src/main/java/projet/bananaspoker/metier/Jeton.java
+++ src/main/java/projet/bananaspoker/metier/Jeton.java
@@ -1,5 +1,32 @@
 package projet.bananaspoker.metier;
 
 public class Jeton {
-	
+
+    private int valeur;
+
+    public Jeton(int val) {
+        this.valeur = val;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Jeton jeton = (Jeton) object;
+        return valeur == jeton.valeur;
+    }
+
+    public java.lang.String toString() {
+        return "Jeton{" +
+                "valeur=" + valeur +
+                "}\n";
+    }
 }

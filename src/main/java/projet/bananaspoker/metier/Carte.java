de0da54475e850f8d522b4fc54a0efb4547425d8
Trystan Baillobay <baillobaytrystan06@gmail.com>
2024-01-26 09:46:44 +0100

    Changements des types des valeurs, méthodes genererJeu() et compareTo()

--- src/main/java/projet/bananaspoker/metier/Carte.java
+++ src/main/java/projet/bananaspoker/metier/Carte.java
@@ -1,45 +1,69 @@
 package projet.bananaspoker.metier;
 
 
-public class Carte {
+public class Carte implements Comparable{
 
 	public final static char COEUR   = '♥';
 	public final static char CARREAU = '♦';
 	public final static char TREFLE  = '♣';
 	public final static char PIQUE   = '♠';
 
-	private char coul;
-	private char valeur;
-	private char signe;
+	private char   couleur;
+	private int    valeur;
+	private String nom;
 
-	public Carte(char coul, char valeur, char signe) {
-		this.coul = coul;
+	public Carte(char couleur, int valeur, String nom) {
+		this.couleur = couleur;
 		this.valeur = valeur;
-		this.signe = signe;
+		this.nom = nom;
 	}
 
-	public char getCoul() {
-		return coul;
+	public static Carte[] genererJeu()
+	{
+		Carte[] jeu = new Carte[52];
+		int[] ensValeurs = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		String[] ensNomsValeurs = {"As","2","3","4","5","6","7","8","9","10","Valet","Dame","Roi"};
+		char[] ensCouleurs = {COEUR,CARREAU,TREFLE,PIQUE};
+		String[] ensNomsCouleurs = {"coeur","carreau","trefle","pique"};
+
+		int nbCartes = 0;
+		for (int cptCouleur = 0; cptCouleur < 4; cptCouleur++)
+		{
+			for (int cptValeur = 0; cptValeur < 13; cptValeur++)
+			{
+				int    val  = ensValeurs[cptValeur];
+				char   coul = ensCouleurs[cptCouleur];
+				String nom  = ensNomsValeurs[cptValeur] + " de " + ensNomsCouleurs[cptCouleur];
+				jeu[nbCartes] = new Carte(coul, val, nom);
+				nbCartes++;
+			}
+		}
+
+		return jeu;
 	}
 
-	public void setCoul(char coul) {
-		this.coul = coul;
+	public char getCouleur() {
+		return couleur;
 	}
 
-	public char getValeur() {
+	public void setCouleur(char coul) {
+		this.couleur = coul;
+	}
+
+	public int getValeur() {
 		return valeur;
 	}
 
-	public void setValeur(char valeur) {
+	public void setValeur(int valeur) {
 		this.valeur = valeur;
 	}
 
-	public char getSigne() {
-		return signe;
+	public String getNom() {
+		return nom;
 	}
 
-	public void setSigne(char signe) {
-		this.signe = signe;
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 
 	public boolean equals(Object object) {
@@ -47,14 +71,17 @@ public class Carte {
 		if (object == null || getClass() != object.getClass()) return false;
 		if (!super.equals(object)) return false;
 		Carte carte = (Carte) object;
-		return coul == carte.coul && valeur == carte.valeur && signe == carte.signe;
+		return couleur == carte.couleur && valeur == carte.valeur;
 	}
 
 	public java.lang.String toString() {
-		return "Carte{" +
-				"coul=" + coul +
-				", valeur=" + valeur +
-				", signe=" + signe +
-				"}\n";
+		return this.nom;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		int[] ensPoints = {13,1,2,3,4,5,6,7,8,9,10,11,12};
+		Carte autreCarte = (Carte) o;
+		return ensPoints[this.valeur-1] - ensPoints[autreCarte.getValeur()-1];
 	}
 }

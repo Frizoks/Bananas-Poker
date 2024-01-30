1c13b03b73ac3b0235c8c5fd79a1a7cc5bac6f7b
Trystan Baillobay <baillobaytrystan06@gmail.com>
2024-01-30 10:37:58 +0100

    Méthode departageJoueurs finie, petites modifications et ajouts de méthodes supplémentaires pour départager correctement les joueurs

--- src/main/java/projet/bananaspoker/metier/Combinaison.java
+++ src/main/java/projet/bananaspoker/metier/Combinaison.java
@@ -8,6 +8,7 @@ public class Combinaison
     private static ArrayList<Carte> main;
     private static char             couleurCarte;
     private static int              valeurCarte;
+    private static int              carteHauteQuinte;
 
     public static String getCombinaison (ArrayList<Carte> main)
     {
@@ -84,19 +85,29 @@ public class Combinaison
                 cptQuinte++;
             }
         }
-
+        carteHauteQuinte = valeursCartes.get(valeursCartes.size()-1);
         return cptQuinte >= 4;
     }
+
+
 
     private static boolean estCouleur()
     {
         // Premier test si il y a une couleur, renseigné ici un char différent de toutes couleurs disponibles
-        return estCouleur('z');
+        return estCouleur('z', main);
     }
-    private static boolean estCouleur(char autrecoul)
+    private static boolean estCouleur(char autreCoul)
+    {
+        return estCouleur(autreCoul, main);
+    }
+    private static boolean estCouleur(ArrayList<Carte> ensCartes)
+    {
+        return estCouleur('z', ensCartes);
+    }
+
+    private static boolean estCouleur(char autrecoul, ArrayList<Carte> ensCartes)
     {
         // autrecoul sert à savoir si il y a une autre combinaison de couleurs dans la main
-
         for (int cpt1 = 0; cpt1 < main.size()-1; cpt1++)
         {
             for (int cpt2 = cpt1+1; cpt2 < main.size(); cpt2++)
@@ -139,20 +150,28 @@ public class Combinaison
 
     private static boolean estPaire()
     {
-        // Premier test si il y a une couleur, renseigné ici un char différent de toutes couleurs disponibles
-        return estPaire(50);
+        return estPaire(main, 50);
     }
-    private static boolean estPaire(int autreValeur)
+    private static boolean estPaire(ArrayList<Carte> ensCartes)
+    {
+        // Premier test si il y a une couleur, renseigné ici un char différent de toutes couleurs disponibles
+        return estPaire(ensCartes, 50);
+    }
+    private static boolean estPaire (int autreValeur)
+    {
+        return estPaire(main, autreValeur);
+    }
+    private static boolean estPaire(ArrayList<Carte> ensCartes, int autreValeur)
     {
         // autrecoul sert à savoir si il y a une autre combinaison de couleurs dans la main
 
-        for (int cpt1 = 0; cpt1 < main.size()-1; cpt1++)
+        for (int cpt1 = 0; cpt1 < ensCartes.size()-1; cpt1++)
         {
-            for (int cpt2 = cpt1+1; cpt2 < main.size(); cpt2++)
+            for (int cpt2 = cpt1+1; cpt2 < ensCartes.size(); cpt2++)
             {
-                if ( (main.get(cpt1).getValeur() == main.get(cpt2).getValeur()) && main.get(cpt1).getValeur() != autreValeur )
+                if ( (ensCartes.get(cpt1).getValeur() == ensCartes.get(cpt2).getValeur()) && ensCartes.get(cpt1).getValeur() != autreValeur )
                 {
-                    Combinaison.valeurCarte = main.get(cpt1).getValeur();
+                    Combinaison.valeurCarte = ensCartes.get(cpt1).getValeur();
                     return true;
                 }
             }
@@ -195,8 +214,37 @@ public class Combinaison
     private static int carteHaute(ArrayList<Carte> ensCartes)
     {
         ArrayList<Integer> valeursCartes = new ArrayList<>();
-        for (Carte carte : main) {
+        for (Carte carte : ensCartes)
+        {
             valeursCartes.add(carte.getValeur());
+        }
+        Collections.sort(valeursCartes);
+
+        return valeursCartes.get(valeursCartes.size() - 1);
+    }
+
+    private static int carteHautePaire(ArrayList<Carte> ensCartes)
+    {
+        ArrayList<Integer> valeursCartes = new ArrayList<>();
+        boolean bT = estPaire(ensCartes);
+        for (Carte carte : ensCartes)
+        {
+            if (carte.getValeur() == valeurCarte)
+                valeursCartes.add(carte.getValeur());
+        }
+        Collections.sort(valeursCartes);
+
+        return valeursCartes.get(valeursCartes.size() - 1);
+    }
+
+    private static int getAcolyte(ArrayList<Carte> ensCartes)
+    {
+        ArrayList<Integer> valeursCartes = new ArrayList<>();
+        boolean bT = estPaire(ensCartes);
+        for (Carte carte : ensCartes)
+        {
+            if (carte.getValeur() != valeurCarte)
+                valeursCartes.add(carte.getValeur());
         }
         Collections.sort(valeursCartes);
 
@@ -224,6 +272,7 @@ public class Combinaison
         ArrayList<Carte> combJ1 = j1.getCombinaisonJoueur();
         ArrayList<Carte> combJ2 = j2.getCombinaisonJoueur();
 
+        //quinte flush royale
         if (estQuinteFlushRoyale(combJ1) && !estQuinteFlushRoyale(combJ2))
             return j1.getNomJoueur() + " gagne !";
         if (estQuinteFlushRoyale(combJ2) && !estQuinteFlushRoyale(combJ1))
@@ -231,36 +280,176 @@ public class Combinaison
         if (estQuinteFlushRoyale(combJ1) &&  estQuinteFlushRoyale(combJ2))
             return "Egalite";
 
+        //quinte flush
         if (estQuinteFlush(combJ1) && !estQuinteFlush(combJ2))
             return j1.getNomJoueur() + " gagne !";
         if (estQuinteFlush(combJ2) && !estQuinteFlush(combJ1))
             return j2.getNomJoueur() + " gagne !";
         if (estQuinteFlush(combJ1) &&  estQuinteFlush(combJ2))
-            return departageCarteHaute(j1, j2);
+            return departageCarteHaute(combJ1, combJ2, j1, j2);
 
+        // carre (paire 4)
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 4) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 4) )
+            return j1.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 4) && !(estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 4) )
+            return j2.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 4) &&  (estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 4) )
+            return departageCarteHauteCarre(j1, j2);
+
+        // full (paire 3 + paire 2)
+        int valP1J1 = 0;
+        int valP2J1 = 0;
+        int valP1J2 = 0;
+        int valP2J2 = 0;
+
+        if (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 3)
+        {
+            valP1J1 = valeurCarte;
+            if (estPaire(combJ1,valP1J1) && nbCarteIdentique(valeurCarte) == 2)
+            {
+                valP2J1 = valeurCarte;
+            }
+        }
+        if (estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 3)
+        {
+            valP1J2 = valeurCarte;
+            if (estPaire(combJ2,valP1J2) && nbCarteIdentique(valeurCarte) == 2)
+            {
+                valP2J2 = valeurCarte;
+            }
+        }
+        if (valP1J1 != 0 && valP1J2 != 0 && valP2J1 != 0 && valP2J2 != 0)
+        {
+            if (valP1J1 > valP1J2)
+                return j1.getNomJoueur() + " gagne !";
+            else if (valP1J2 > valP1J1)
+                return j2.getNomJoueur() + " gagne !";
+            else if (valP2J1 > valP2J2)
+                return j1.getNomJoueur() + " gagne !";
+            else if (valP2J2 > valP2J1)
+                return j2.getNomJoueur() + " gagne !";
+            else
+                return "Egalité parfaite";
+        }
+
+        // couleur 5
+        if ( (estCouleur(combJ1) && nbCouleur(couleurCarte) == 5) && !(estCouleur(combJ2) && nbCouleur(couleurCarte) == 5) )
+            return j1.getNomJoueur() + " gagne !";
+        if ( (estCouleur(combJ2) && nbCouleur(couleurCarte) == 5) && !(estCouleur(combJ1) && nbCouleur(couleurCarte) == 5) )
+            return j2.getNomJoueur() + " gagne !";
+        if ( (estCouleur(combJ1) && nbCouleur(couleurCarte) == 5) &&  (estCouleur(combJ2) && nbCouleur(couleurCarte) == 5) )
+            return departageCarteHaute(combJ1, combJ2, j1, j2);
+
+        //quinte
         if (estQuinte(combJ1) && !estQuinte(combJ2))
             return j1.getNomJoueur() + " gagne !";
         if (estQuinte(combJ2) && !estQuinte(combJ1))
             return j2.getNomJoueur() + " gagne !";
         if (estQuinte(combJ1) &&  estQuinte(combJ2))
-            return departageCarteHaute(j1, j2);
+            return departageCarteHaute(combJ1, combJ2, j1, j2);
 
+        // brelan (paire 3)
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 3) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 3) )
+            return j1.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 3) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 3) )
+            return j2.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 3) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 3) )
+            return departageCarteHaute(combJ1, combJ2, j1, j2);
 
+        // 2 paires (paire 2 + paire 2)
+        if (estPaire(combJ1) && estPaire(combJ1, valeurCarte) && estPaire(combJ2) && estPaire(combJ2, valeurCarte))
+            return departageDoublePaire(combJ1, combJ2, j1, j2);
 
-        return departageCarteHaute(j1, j2);
+        // 1 paire (paire 2)
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 2) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 2) )
+            return j1.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 2) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 2) )
+            return j2.getNomJoueur() + " gagne !";
+        if ( (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 2) && !(estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 2) )
+            return departageCarteHaute(combJ1, combJ2, j1, j2);
+
+        return departageCarteHaute(combJ1, combJ2, j1, j2);
     }
 
-    public String departageCarteHaute(Joueur j1, Joueur j2)
+    private String departageCarteHaute(ArrayList<Carte> combJ1, ArrayList<Carte> combJ2, Joueur j1, Joueur j2)
+    {
+        if (!combJ1.isEmpty())
+        {
+            if (carteHaute(combJ1) > carteHaute(combJ2))
+                return j1.getNomJoueur() + " gagne !";
+            else if (carteHaute(combJ1) < carteHaute(combJ2))
+                return j2.getNomJoueur() + " gagne !";
+            else {
+                for (int cpt = 0; cpt < combJ1.size(); cpt++)
+                    if (combJ1.get(cpt).getValeur() == carteHaute(combJ1))
+                        combJ1.remove(cpt);
+
+                for (int cpt = 0; cpt < combJ2.size(); cpt++)
+                    if (combJ2.get(cpt).getValeur() == carteHaute(combJ2))
+                        combJ2.remove(cpt);
+
+                return departageCarteHaute(combJ1, combJ2, j1, j2);
+            }
+        }
+        else
+        {
+            return "Egalité parfaite";
+        }
+    }
+
+    private String departageCarteHauteCarre(Joueur j1, Joueur j2)
     {
         ArrayList<Carte> combJ1 = j1.getCombinaisonJoueur();
         ArrayList<Carte> combJ2 = j2.getCombinaisonJoueur();
 
-        if (carteHaute(combJ1) > carteHaute(combJ2))
+        if (carteHautePaire(combJ1) > carteHautePaire(combJ2))
             return j1.getNomJoueur() + " gagne !";
-        else if (carteHaute(combJ1) < carteHaute(combJ2))
+        else if (carteHautePaire(combJ2) > carteHautePaire(combJ1))
+            return j2.getNomJoueur() + " gagne !";
+            // "Si les deux joueurs ont le même carré, le pot est remis à celui qui a la cinquième carte la plus haute, appelée aussi acolyte"
+        else if (getAcolyte(combJ1) > getAcolyte(combJ2))
+            return j1.getNomJoueur() + " gagne !";
+        else if (getAcolyte(combJ2) > getAcolyte(combJ1))
             return j2.getNomJoueur() + " gagne !";
         else
             return "Egalité parfaite";
+    }
+
+    private String departageDoublePaire(ArrayList<Carte> combJ1, ArrayList<Carte> combJ2, Joueur j1, Joueur j2)
+    {
+        int valP1J1 = 0;
+        int valP2J1 = 0;
+        int valP1J2 = 0;
+        int valP2J2 = 0;
+
+        if (estPaire(combJ1) && nbCarteIdentique(valeurCarte) == 2)
+        {
+            valP1J1 = valeurCarte;
+            if (estPaire(combJ1,valP1J1) && nbCarteIdentique(valeurCarte) == 2)
+            {
+                valP2J1 = valeurCarte;
+            }
+        }
+        if (estPaire(combJ2) && nbCarteIdentique(valeurCarte) == 2)
+        {
+            valP1J2 = valeurCarte;
+            if (estPaire(combJ2,valP1J2) && nbCarteIdentique(valeurCarte) == 2)
+            {
+                valP2J2 = valeurCarte;
+            }
+        }
+        if (valP1J1 != 0 && valP1J2 != 0 && valP2J1 != 0 && valP2J2 != 0)
+        {
+            if (valP1J1 > valP1J2 && valP1J1 > valP2J2)
+                return j1.getNomJoueur() + " gagne !";
+            else if (valP1J2 > valP1J1 && valP1J2 > valP2J1)
+                return j2.getNomJoueur() + " gagne !";
+            else if (valP2J1 > valP2J2 && valP2J1 > valP1J2)
+                return j1.getNomJoueur() + " gagne !";
+            else if (valP2J2 > valP2J1 && valP2J2 > valP1J2)
+                return j2.getNomJoueur() + " gagne !";
+        }
+        return "Egalité parfaite";
     }
 
     /*public String gestionPaire()

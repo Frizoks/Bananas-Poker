f7b66dff168bdecc104d2052f5c02380a7e66579
Trystan Baillobay <baillobaytrystan06@gmail.com>
2024-02-09 17:08:07 +0100

    Tabulations et méthode pour set la meilleure combinaison possible du joueur

--- src/main/java/projet/bananaspoker/metier/Joueur.java
+++ src/main/java/projet/bananaspoker/metier/Joueur.java
@@ -8,96 +8,96 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Joueur {
-	private String nomJoueur;
-	private ArrayList<Carte> mainJoueur;
-	private ArrayList<Carte> combinaisonJoueur;
-	private int nbJetonsJoueur;
-	private PrintWriter sortie;
-	private BufferedReader entree;
+    private String nomJoueur;
+    private ArrayList<Carte> mainJoueur;
+    private ArrayList<Carte> combinaisonJoueur;
+    private int nbJetonsJoueur;
+    private PrintWriter sortie;
+    private BufferedReader entree;
 
-	public Joueur(String nomJoueur, ArrayList<Carte> mainJoueur, int nbJetonsJoueur) {
-		this.nomJoueur = nomJoueur;
-		this.mainJoueur = mainJoueur;
-		this.combinaisonJoueur = Combinaison.determineCombinaisonJoueur(this);
-		this.nbJetonsJoueur = nbJetonsJoueur;
-	}
+    public Joueur(String nomJoueur, ArrayList<Carte> mainJoueur, int nbJetonsJoueur) {
+        this.nomJoueur = nomJoueur;
+        this.mainJoueur = mainJoueur;
+        this.combinaisonJoueur = Combinaison.setCombinaisonJoueur(this);
+        this.nbJetonsJoueur = nbJetonsJoueur;
+    }
 
-	public Joueur(String nomJoueur, int nbJetonsJoueur) {
-		this.nomJoueur = nomJoueur;
-		this.nbJetonsJoueur = nbJetonsJoueur;
-		this.entree = null;
-		this.sortie = null;
-	}
+    public Joueur(String nomJoueur, int nbJetonsJoueur) {
+        this.nomJoueur = nomJoueur;
+        this.nbJetonsJoueur = nbJetonsJoueur;
+        this.entree = null;
+        this.sortie = null;
+    }
 
-	public void setPorts(Socket clientSocket) throws IOException {
-		this.sortie = new PrintWriter(clientSocket.getOutputStream(), true);
-		this.entree = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	}
+    public void setPorts(Socket clientSocket) throws IOException {
+        this.sortie = new PrintWriter(clientSocket.getOutputStream(), true);
+        this.entree = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
 
-	public String getNomJoueur() {
-		return nomJoueur;
-	}
+    public String getNomJoueur() {
+        return nomJoueur;
+    }
 
-	public void setNomJoueur(String nomJoueur) {
-		this.nomJoueur = nomJoueur;
-	}
+    public void setNomJoueur(String nomJoueur) {
+        this.nomJoueur = nomJoueur;
+    }
 
-	public ArrayList<Carte> getMainJoueur() {
-		return mainJoueur;
-	}
+    public ArrayList<Carte> getMainJoueur() {
+        return mainJoueur;
+    }
 
-	public void setMainJoueur(ArrayList<Carte> mainJoueur) {
-		this.mainJoueur = mainJoueur;
-	}
+    public void setMainJoueur(ArrayList<Carte> mainJoueur) {
+        this.mainJoueur = mainJoueur;
+    }
 
-	public void addCarteMainJoueur(Carte c) {
-		if (this.mainJoueur.size() < 2) {
-			this.mainJoueur.add(c);
-		}
-	}
+    public void addCarteMainJoueur(Carte c) {
+        if (this.mainJoueur.size() < 2) {
+            this.mainJoueur.add(c);
+        }
+    }
 
-	public int getNbJetonsJoueur() {
-		return nbJetonsJoueur;
-	}
+    public int getNbJetonsJoueur() {
+        return nbJetonsJoueur;
+    }
 
-	public void setNbJetonsJoueur(int nbJetonsJoueur) {
-		this.nbJetonsJoueur = nbJetonsJoueur;
-	}
+    public void setNbJetonsJoueur(int nbJetonsJoueur) {
+        this.nbJetonsJoueur = nbJetonsJoueur;
+    }
 
-	public int enleverJetons(int nbJetons) {
-		if (this.nbJetonsJoueur - nbJetons > 0) {
-			this.nbJetonsJoueur -= nbJetons;
-			return nbJetons;
-		} else if (this.nbJetonsJoueur - nbJetons == 0) {
-			this.nbJetonsJoueur = 0;
-			return 0;
-		} else {
-			this.nbJetonsJoueur = 0;
-			return -1;
-		}
-	}
+    public int enleverJetons(int nbJetons) {
+        if (this.nbJetonsJoueur - nbJetons > 0) {
+            this.nbJetonsJoueur -= nbJetons;
+            return nbJetons;
+        } else if (this.nbJetonsJoueur - nbJetons == 0) {
+            this.nbJetonsJoueur = 0;
+            return 0;
+        } else {
+            this.nbJetonsJoueur = 0;
+            return -1;
+        }
+    }
 
-	public void ajouterJetons(int nbJetons) {
-		this.nbJetonsJoueur += nbJetons;
-	}
+    public void ajouterJetons(int nbJetons) {
+        this.nbJetonsJoueur += nbJetons;
+    }
 
-	public ArrayList<Carte> getCombinaisonJoueur() {
-		return combinaisonJoueur;
-	}
+    public ArrayList<Carte> getCombinaisonJoueur() {
+        return combinaisonJoueur;
+    }
 
-	public void setCombinaisonJoueur(ArrayList<Carte> combinaisonJoueur) {
-		this.combinaisonJoueur = combinaisonJoueur;
-	}
+    public void setCombinaisonJoueur(ArrayList<Carte> combinaisonJoueur) {
+        this.combinaisonJoueur = combinaisonJoueur;
+    }
 
-	public PrintWriter getSortie() {
-		return this.sortie;
-	}
+    public PrintWriter getSortie() {
+        return this.sortie;
+    }
 
-	public BufferedReader getEntree() {
-		return this.entree;
-	}
+    public BufferedReader getEntree() {
+        return this.entree;
+    }
 
-	public String toString() {
-		return this.nomJoueur + ":" + this.nbJetonsJoueur;
-	}
+    public String toString() {
+        return this.nomJoueur + ":" + this.nbJetonsJoueur;
+    }
 }
